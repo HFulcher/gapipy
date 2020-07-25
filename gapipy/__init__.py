269d2c0e677f1d068763f50f859b4a382efd8885
Huw Fulcher <fulcherhuw@gmail.com>
2020-07-25 23:08:14 +0100

    Clear through linting issues Fixes #2

--- gapipy/__init__.py
+++ gapipy/__init__.py
@@ -2,6 +2,7 @@
 
 import pkg_resources
 
-from . import auth, commands, tests, utils, account, auth, blueprint, columns, errors, query, segments
+from . import auth, commands, tests, utils, account, auth, blueprint, columns, 
+                errors, query, segments
 from .auth import authenticate
 from .blueprint import Blueprint
--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@ import nox
 
 locations = "gapipy", "noxfile.py"
 
+
 @nox.session
 def tests(session):
     session.install("-r", "requirements.txt")
@@ -9,14 +10,17 @@ def tests(session):
     session.install('pytest')
     session.run('pytest')
 
+
 @nox.session
 def lint(session):
     session.install('flake8')
     session.run('flake8', *locations)
 
+
 @nox.session
 def docs(session):
-    session.install('sphinx', 'sphinx-autodoc-typehints', 'sphinx-rtd-theme', '-r', 'requirements.txt')
+    session.install('sphinx', 'sphinx-autodoc-typehints', 'sphinx-rtd-theme',
+                    '-r', 'requirements.txt')
     session.install('.')
     session.run('sphinx-apidoc', '-o', 'docs/', 'gapipy')
     session.run('make', '-C', 'docs', 'html', external=True)
