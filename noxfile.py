74ddd0aaea438a92f79b77473ebf1486910e62d1
Huw Fulcher <fulcherhuw@gmail.com>
2020-07-25 23:59:28 +0100

    Adding code coverage

--- noxfile.py
+++ noxfile.py
@@ -24,3 +24,10 @@ def docs(session):
     session.install('.')
     session.run('sphinx-apidoc', '-o', 'docs/', 'gapipy')
     session.run('make', '-C', 'docs', 'html', external=True)
+
+
+@nox.session
+def coverage(session):
+    session.install("coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
