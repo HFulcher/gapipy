caad31fea3bd462f085adad73401d03486065926
Huw Fulcher <fulcherhuw@gmail.com>
2020-07-25 23:12:52 +0100

    Resolving import issue for sphinx

--- gapipy/__init__.py
+++ gapipy/__init__.py
@@ -3,6 +3,6 @@
 import pkg_resources
 
 from . import auth, commands, tests, utils, account, auth, blueprint, columns, 
-                errors, query, segments
+    errors, query, segments
 from .auth import authenticate
 from .blueprint import Blueprint
