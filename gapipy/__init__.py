a9425841bb17d70338b7fcac4db5cfbe01143b62
Huw Fulcher <fulcherhuw@gmail.com>
2020-07-28 10:49:01 +0100

    Fixing import error

--- gapipy/__init__.py
+++ gapipy/__init__.py
@@ -1,7 +1,10 @@
 # encoding: utf-8
+__title__ = "gapipy"
+__author__ = "Huw Fulcher"
 
 import pkg_resources
 
-from . import (client, tests, utils, account, blueprint, columns, 
-errors, query, segments)
+from . import (client, tests, utils, account, blueprint, columns, errors, query,
+segments)
 from .blueprint import Blueprint
+from .query import Response
